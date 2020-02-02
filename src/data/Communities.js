3ca30e1f64d30630f798e640f685d5aa69db9cc4
Gianluca Esposito <g@esposi.to>
2020-02-02 22:16:03 +0100

    Update links

--- src/data/Communities.js
+++ src/data/Communities.js
@@ -1,51 +1,51 @@
-import logoNA from '../images/NA-DEV-DAY_logo.png';
-import logoSA from '../images/SA-DEV-DAY_logo.png';
-import logoBN from '../images/BN-DEV-DAY_logo.png';
-import logoAV from '../images/AV-DEV-DAY_logo.png';
-import logoCE from '../images/CE-DEV-DAY_logo.png';
+import logoNA from "../images/NA-DEV-DAY_logo.png";
+import logoSA from "../images/SA-DEV-DAY_logo.png";
+import logoBN from "../images/BN-DEV-DAY_logo.png";
+import logoAV from "../images/AV-DEV-DAY_logo.png";
+import logoCE from "../images/CE-DEV-DAY_logo.png";
 //import logoMAG from '../images/MAKEAGAME.png';
 
 var Communities = [
-    {
-        name: "DevDay Napoli",
-        color: "blue",
-        logo: logoNA,
-        description: "Visita la pagina della comunità napoletana!",
-        facebook: "https://www.facebook.com/groups/devday/",
-        eventbrite: "http://napoli.devday.it"
-    },
-    {
-        name: "DevDay Salerno",
-        color: "red",
-        logo: logoSA,
-        description: "Visita la pagina della comunità salernitana!",
-        facebook: "https://www.facebook.com/groups/912007198920223/",
-        meetup: "http://salerno.devday.it"
-    },
-    {
-        name: "DevDay Avellino",
-        color: "green",
-        logo: logoAV,
-        description: "Visita la pagina della comunità avellinese!",
-        facebook: "https://www.facebook.com/groups/1187130324674292/",
-        meetup: "http://avellino.devday.it"
-    },
-    {
-        name: "DevDay Benevento",
-        color: "yellow",
-        logo: logoBN,
-        description: "Visita la pagina della comunità beneventana!",
-        facebook: "https://www.facebook.com/groups/devdaybn/",
-        meetup: "http://benevento.devday.it"
-    },
-    {
-        name: "DevDay Caserta",
-        color: "blue",
-        logo: logoCE,
-        description: "Visita la pagina della comunità casertana!",
-        facebook: "https://www.facebook.com/groups/290923128043281/",
-        meetup: "https://www.meetup.com/devday-caserta"
-    }
+  {
+    name: "DevDay Napoli",
+    color: "blue",
+    logo: logoNA,
+    description: "Visita la pagina della comunità napoletana!",
+    facebook: "https://www.facebook.com/groups/devday/",
+    meetup: "https://napoli.devday.it"
+  },
+  {
+    name: "DevDay Salerno",
+    color: "red",
+    logo: logoSA,
+    description: "Visita la pagina della comunità salernitana!",
+    facebook: "https://www.facebook.com/groups/912007198920223/",
+    meetup: "https://salerno.devday.it"
+  },
+  {
+    name: "DevDay Avellino",
+    color: "green",
+    logo: logoAV,
+    description: "Visita la pagina della comunità avellinese!",
+    facebook: "https://www.facebook.com/groups/1187130324674292/",
+    meetup: "https://avellino.devday.it"
+  },
+  {
+    name: "DevDay Benevento",
+    color: "yellow",
+    logo: logoBN,
+    description: "Visita la pagina della comunità beneventana!",
+    facebook: "https://www.facebook.com/groups/devdaybn/",
+    meetup: "https://benevento.devday.it"
+  },
+  {
+    name: "DevDay Caserta",
+    color: "blue",
+    logo: logoCE,
+    description: "Visita la pagina della comunità casertana!",
+    facebook: "https://www.facebook.com/groups/290923128043281/",
+    meetup: "https://www.meetup.com/devday-caserta"
+  }
 ];
 
 export default Communities;
